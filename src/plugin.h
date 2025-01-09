52cbd046c185f9f47a92b8f81b9fc4b7ce48b501
Simon Long <simon@raspberrypi.com>
2025-01-09 14:42:28 +0000

    Add generic graph code

--- src/plugin.h
+++ src/plugin.h
@@ -396,6 +396,24 @@ extern void lxpanel_notify_init (LXPanel *panel);
 extern int lxpanel_notify (LXPanel *panel, char *message);
 extern void lxpanel_notify_clear (int seq);
 
+#define BORDER_SIZE 2
+
+typedef struct {
+    GtkWidget *da;                          /* Drawing area */
+    cairo_surface_t *pixmap;                /* Pixmap to be drawn on drawing area */
+    float *samples;                         /* Ring buffer of values */
+    int *samp_states;                       /* Ring buffer of states used for colours */
+    unsigned int ring_cursor;               /* Cursor for ring buffer */
+    guint pixmap_width;                     /* Width of drawing area pixmap; also size of ring buffer; does not include border size */
+    guint pixmap_height;                    /* Height of drawing area pixmap; does not include border size */
+    GdkRGBA background;                     /* Graph background colour */
+    GdkRGBA colours[3];                     /* Graph foreground colours - normal and throttled */
+} PluginGraph;
+
+extern void graph_init (PluginGraph *graph);
+extern void graph_reload (PluginGraph *graph, int icon_size, GdkRGBA background, GdkRGBA foreground, GdkRGBA throttle1, GdkRGBA throttle2);
+extern void graph_new_point (PluginGraph *graph, float value, int state, char *label);
+
 G_END_DECLS
 
 #undef _
